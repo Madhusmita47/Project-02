b6efe36aee760179f4085876e14bb85f5e9aeaa3
Madhusmita47 <madhusmita238091@gmail.com>
2022-11-21 17:09:38 +0530

    added code for validation

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,54 @@
 const internModel = require('../models/internModel')
 const collegeModel = require('../models/collegeModel')
+const internModel=("../models/internModel.js")
+const { value,regForName,regForFullName,regForLInk,regForEmail,regForMobileNo}=require("../validation/validation.js")
+
+const createCollege=async function(req,res){
+    try{
+        let data=req.body
+        let {name,fullName,logoLink}=data
+        //---------checking the mandatory fields------------------
+        if(!(name && fullName && logoLink))
+        return res.status(400).send({status:false,message:"all fields are mandatory"})
+        //----checkduplicatevalue------
+        let checkDuplicate=await findOne({name:data.name})
+        if(checkDuplicate)
+        return res.status(400).send({status:false,message:"the name is exist please provide another college name"})
+
+        //-------validation of name------------
+        if(!(valid(name))) return res.status(400).send({status:false,message:"provide a valid name"})
+        if(!(regForName(name))) return res.status(400).send({status:false,message:"invalid name"})
+        //---------validation of fullname---------
+        if(!(valid(fullName))) return res.status(400).send({status:false,message:"provide a valid fullname"})
+        if(!(regForFullName(regForFullName))) return res.status(400).send({status:false,message:"invalid fullname first letter should be in upper case"})
+        //------validation of logo link---------
+        if(!(valid(logoLink))) return res.status(400).send({status:false,message:"provide a valid logo link"})
+        if(!(valid(regForLInk))) return res.status(400).send({status:false,message:"invalid link"})
+        //----validation 
+
+       //-------creating college data-------------
+    let collegeData=await collegeModel.create(data)
+   //let obj={name:collegeSchema.name,fullName:collegeSchema.fullName,logoLink:collegeSchema.logoLink,isDeleted:collegeSchema.isDeleted}
+    res.status(201).send({status:true,data:collegeData})
+    }
+    catch(error){
+        return res.status(500).send({status:false,message:error.messege})
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //3
@@ -23,3 +72,5 @@ const Handler3 = async function (req, res) {
 }
 
 
+
+module.exports=createCollege
---
+++ src/routes/route.js
@@ -0,0 +1,17 @@
+const express=require("express")
+const router=express.Router()
+const {createCollege}=require("../controllers/collegeController")
+
+router.post("/functionup/colleges", createCollege)
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router; 
