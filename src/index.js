932294353a35d988d768eaddb941b2568f491e61
akashGg99 <aakkash15r@gmail.com>
2022-11-21 15:18:08 +0530

    created project structure

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose')
+const route = require("../routes/routes")
+const app = express()
+
+
+
+app.use(bodyparser.json())
+// app.use(bodyparser.urlencoded)
+
+mongoose.connect("mongodb+srv://divyajeet:12345@project2.wp73hk4.mongodb.net/Project_2", {useNewUrlParser:true} )
+.then(() => console.log("MongoDB connected.."))
+.catch(err => console.log(err))
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
